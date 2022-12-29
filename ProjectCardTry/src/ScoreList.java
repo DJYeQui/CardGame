038997d8d59e64919e8294aa07f0a8a0ab112591
Liande <114687234+DJYeQui@users.noreply.github.com>
2022-12-29 19:54:55 +0300

    some implamentation added

--- ProjectCardTry/src/ScoreList.java
+++ ProjectCardTry/src/ScoreList.java
@@ -1,8 +1,9 @@
 import java.io.IOException;
-import java.util.Scanner ;
-import java.util.Formatter ;
-import java.io.FileWriter ;
+import java.util.Scanner;
+import java.util.Formatter;
+import java.io.FileWriter;
 import java.nio.file.Paths;
+
 /* In the people.txt it has to be 20 zero like that
 0 0
 0 0
@@ -17,11 +18,12 @@ import java.nio.file.Paths;
 */
 public class ScoreList {
     private int playerScore;
+
     public ScoreList(int pScore) {
         playerScore = pScore;
     }
 
-    public void scoreList(){
+    public void scoreList() {
 
         // copy  old list values from txt file
         String[][] listPlayerNamePoint = new String[11][2];
@@ -29,57 +31,56 @@ public class ScoreList {
         System.out.println("pls enter score");
         String playerName = name(); // there was a problem to take name
 
-        listPlayerNamePoint[10][0]=playerName;
-        listPlayerNamePoint[10][1]=Integer.toString(playerScore);
+        listPlayerNamePoint[10][0] = playerName;
+        listPlayerNamePoint[10][1] = Integer.toString(playerScore);
 
         Scanner reader = null;
         try {
             reader = new Scanner(Paths.get("people.txt"));
 
 
+            int counterExecution = 0;
 
-            int counterExecution=0;
-
-            while (reader.hasNextLine()){
+            while (reader.hasNextLine()) {
                 String[] oldListValues = reader.nextLine().split(" ");
-                if (counterExecution<=10){
-                    for (int index = 0;  index<2; index++){
-                        if (listPlayerNamePoint[counterExecution][0]==null && listPlayerNamePoint[counterExecution][1]==null){
-                            listPlayerNamePoint[counterExecution][0]=Integer.toString(0);
-                            listPlayerNamePoint[counterExecution][1]=Integer.toString(0);
+                if (counterExecution <= 10) {
+                    for (int index = 0; index < 2; index++) {
+                        if (listPlayerNamePoint[counterExecution][0] == null && listPlayerNamePoint[counterExecution][1] == null) {
+                            listPlayerNamePoint[counterExecution][0] = Integer.toString(0);
+                            listPlayerNamePoint[counterExecution][1] = Integer.toString(0);
                         }
-                        listPlayerNamePoint[counterExecution][index]=oldListValues[index];
+                        listPlayerNamePoint[counterExecution][index] = oldListValues[index];
                     }
                 }
                 counterExecution++;
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();    // for see error's detail
-        }finally {
-            if (reader!=null){
+        } finally {
+            if (reader != null) {
                 reader.close();
             }
         }
 
-        for (int i = 0; i<11; i++){
-            for (int j = 0; j<11; j++){
-                if (listPlayerNamePoint[i][1]!=null && listPlayerNamePoint[j][1]!=null){
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (listPlayerNamePoint[i][1] != null && listPlayerNamePoint[j][1] != null) {
 
-                    int a=Integer.parseInt(listPlayerNamePoint[i][1]);
-                    int b=Integer.parseInt(listPlayerNamePoint[j][1]);
+                    int a = Integer.parseInt(listPlayerNamePoint[i][1]);
+                    int b = Integer.parseInt(listPlayerNamePoint[j][1]);
 
                     String a2 = listPlayerNamePoint[i][0];
                     String b2 = listPlayerNamePoint[j][0];
 
-                    if (a>b){
-                        String placeHolder= Integer.toString(a);
-                        String placeHolder2= a2;
+                    if (a > b) {
+                        String placeHolder = Integer.toString(a);
+                        String placeHolder2 = a2;
 
-                        listPlayerNamePoint[i][1]=listPlayerNamePoint[j][1];
-                        listPlayerNamePoint[j][1]=placeHolder;
+                        listPlayerNamePoint[i][1] = listPlayerNamePoint[j][1];
+                        listPlayerNamePoint[j][1] = placeHolder;
 
-                        listPlayerNamePoint[i][0]=listPlayerNamePoint[j][0];
-                        listPlayerNamePoint[j][0]=placeHolder2;
+                        listPlayerNamePoint[i][0] = listPlayerNamePoint[j][0];
+                        listPlayerNamePoint[j][0] = placeHolder2;
                     }
                 }
             }
@@ -87,43 +88,46 @@ public class ScoreList {
 
         // control the all print
         System.out.println("================ Score List =================");
-        for (int i = 0; i<11; i++){
-            for (int j = 0; j<2; j++){
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 2; j++) {
                 System.out.println(listPlayerNamePoint[i][j]);
 
             }
         }
         printTxt(listPlayerNamePoint);
     }
-    public static void printTxt(String[][] listInOrder){
+
+    public static void printTxt(String[][] listInOrder) {
 
         Formatter f = null;
         Formatter deleteAllText = null;
         FileWriter fw = null;
         try {
-            fw = new FileWriter ("people.txt" , true);
-            f = new Formatter (fw);
+            fw = new FileWriter("people.txt", true);
+            f = new Formatter(fw);
             deleteAllText = new Formatter("people.txt");
 
-            deleteAllText. format(" ");
+            deleteAllText.format(" ");
 
-            for (int i =0; i<10;i++){
-                f. format("%s %s\n",listInOrder[i][0],listInOrder[i][1]);
+            for (int i = 0; i < 10; i++) {
+                f.format("%s %s\n", listInOrder[i][0], listInOrder[i][1]);
             }
 
-            fw. close();
+            fw.close();
         } catch (Exception e) {
-            System.err.println("Something went wrong." );
+            System.err.println("Something went wrong.");
         } finally {
             if (f != null) {
-                f. close();
+                f.close();
             }
         }
     }
-    public static String  name(){
+
+    public static String name() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("pls enter name");
+        System.out.println("YOU WINN CONG. pls enter your first name");
         String name = sc.nextLine();
-        return name;
+        String[] playerComplexName = name.split(" ");
+        return playerComplexName[0];
     }
 }
